8a9bc2d0d29209397e84bbe814dac7f41e276e53
danghuudong <dongtimo2005@gmail.com>
2024-11-19 18:56:20 +0700

    init project

---
+++ Chuong3/src/B1_person/Test.java
@@ -0,0 +1,20 @@
+package B1_person;
+
+public class Test {
+
+	 public static void main(String[] args) {
+	        Person P1=new Person("Dang Huu Dong","09/05/2005",'M',"dong@gmail.com","0987667786");
+	        System.out.println(P1.getName());
+	        System.out.println(P1.getDob());
+	        System.out.println(P1.getGender());
+	        System.out.println(P1.getEmail());
+	        System.out.println( P1.getPhone());
+	        System.out.println(P1.toString());;
+
+
+
+
+
+
+	    }
+	}
